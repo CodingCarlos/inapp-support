9fde0a2244892a9f91e5d9a631a96c22bd145c83
Carlos Hernandez <carloshdmn@gmail.com>
2016-11-22 19:38:28 +0100

    export config to external js

---
+++ js/firebase.js
@@ -0,0 +1,12 @@
+
+/* PASTE HERE YOUR FIREBASE CONFIG DATA */
+
+// Initialize Firebase
+var config = {
+	apiKey: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+	authDomain: "XXXXX-XXXXX.firebaseapp.com",
+	databaseURL: "https://XXXXX-XXXXX.firebaseio.com",
+	storageBucket: "xxxxx-xxxxx.appspot.com",
+	messagingSenderId: "XXXXXXXXXXXX"
+};
+firebase.initializeApp(config);
