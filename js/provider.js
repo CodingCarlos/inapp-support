2139b3b6ada2ba3ade3e395cc56353db4e164af9
Ulises Gascón <ulisesgascongonzalez@gmail.com>
2016-12-09 18:16:40 +0000

    solución merge

--- js/provider.js
+++ js/provider.js
@@ -1,43 +1,3 @@
-<<<<<<< HEAD
-/*Fill the users list*/
-
-var usersRef = firebase.database().ref('users');
-	
-	usersRef.orderByChild("timestamp").on('child_added', function(snapshot){
-	    var data = snapshot.val();
-	    console.log("data:", data)
-	    //document.getElementById("orderByChild").innerHTML += "<li>" + snapshot.key() + " was " + snapshot.val().height + " meters tall</li>";
-	});
-	
-	userListEvents()
-
-/*
-var ref = new Firebase("https://dinosaur-facts.firebaseio.com/dinosaurs");
-  ref.orderByChild("height").on("child_added", function(snapshot) {
-    console.log(snapshot.key() + " was " + snapshot.val().height + " meters tall");
- });
-*/
-
-/*Set the Events for users list*/
-function userListEvents(){
-    var usersChat = document.getElementsByClassName("users-chat");
-    
-    function usersChatManagement(event) {
-        var event = event || window.event;
-        var element = event.target || event.srcElement;
-        window.IASChat.setUser({
-            cid: element.getAttribute("data-cid"), 
-            uid: element.getAttribute("data-uid")
-        }); 
-        window.IASChat.open(event);
-    };
-    
-    for (var i = 0; i < usersChat.length; i++) {
-        usersChat[i].addEventListener('click', usersChatManagement, false);
-    }
-=======
-
-
 function IASChatProvider(config) {
 
 	var uid = config.uid;
@@ -140,5 +100,4 @@ function IASChatProvider(config) {
 			}
 		});
 	}
->>>>>>> 3f2d44afd79f94e4fb581fcd3dd41145621a9a99
 }
