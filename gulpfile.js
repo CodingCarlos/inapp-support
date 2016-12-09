30912000f6c79d9227f2f3d3e46d6c4120c30f6b
Ulises Gascón <ulisesgascongonzalez@gmail.com>
2016-12-09 21:46:16 +0000

    temporary solution for ./dist - All key files added

--- gulpfile.js
+++ gulpfile.js
@@ -13,4 +13,24 @@ gulp.task('render', function(){
         .pipe(gulp.dest("./dist/js"));
 })
 
-gulp.task('default', ['render']);
+gulp.task('dist-html', function(){
+    gulp.src("./*.html")
+        .pipe(debug({title: 'Move to Dist - HTML:'}))
+        .pipe(gulp.dest("./dist"));
+})
+
+gulp.task('dist-css', function(){
+    gulp.src("./css/*.css")
+        .pipe(debug({title: 'Move to Dist - CSS:'}))
+        .pipe(gulp.dest("./dist/css"));
+})
+
+gulp.task('dist-img', function(){
+    gulp.src("./img/**.**")
+        .pipe(debug({title: 'Move to Dist - IMG:'}))
+        .pipe(gulp.dest("./dist/img"));
+})
+
+gulp.task('move-to-dis', ['dist-html', 'dist-css', 'dist-img'])
+
+gulp.task('default', ['move-to-dis','render']);
