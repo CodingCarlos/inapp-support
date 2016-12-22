270423fe905ff78fd814bac04af7b0f42bf299ab
Ulises Gascón <ulisesgascongonzalez@gmail.com>
2016-12-22 20:38:28 +0000

    Fixes #5

--- template/data.js
+++ template/data.js
@@ -5,7 +5,7 @@ require.extensions['.html'] = function (module, filename) {
 };
 
 module.exports = {
-    ias: require("./ias.html").replace(/(\r\n\t|\n|\r|\t)/gm,"").replace(/(\')/gm,"\\\'"),
+    ias: require("./ias.html").replace(/(\r\n\t|\n|\r|\t)/gm,"").replace(/(\')/gm,"\\\'").replace(/(\")/gm,'\\\"'),
     iasSegment: require("./ias_segment.html").replace(/(\r\n\t|\n|\r|\t)/gm,"").replace(/(\')/gm,"\\'"),
     iasStyle: require("./ias_style.html").replace(/(\r\n\t|\n|\r|\t)/gm,"").replace(/(\')/gm,"\\'"),
     iasProvider: require("./ias_provider.html").replace(/(\r\n\t|\n|\r|\t)/gm,"").replace(/(\')/gm,"\\'")
